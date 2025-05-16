53646568905eeefec15e7535132f7dd993d5306e
xhyh <zwcfather@gmail.com>
2025-05-17 00:10:35 +0800

    remove makefile

---
+++ build/CMakeFiles/DesktopAssistant.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xhyh/useless/DesktopAssistant/modified/src/Crypto.cpp" "CMakeFiles/DesktopAssistant.dir/src/Crypto.cpp.o" "gcc" "CMakeFiles/DesktopAssistant.dir/src/Crypto.cpp.o.d"
+  "/home/xhyh/useless/DesktopAssistant/modified/src/DesktopAssistant.cpp" "CMakeFiles/DesktopAssistant.dir/src/DesktopAssistant.cpp.o" "gcc" "CMakeFiles/DesktopAssistant.dir/src/DesktopAssistant.cpp.o.d"
+  "/home/xhyh/useless/DesktopAssistant/modified/src/Utils.cpp" "CMakeFiles/DesktopAssistant.dir/src/Utils.cpp.o" "gcc" "CMakeFiles/DesktopAssistant.dir/src/Utils.cpp.o.d"
+  "/home/xhyh/useless/DesktopAssistant/modified/src/main.cpp" "CMakeFiles/DesktopAssistant.dir/src/main.cpp.o" "gcc" "CMakeFiles/DesktopAssistant.dir/src/main.cpp.o.d"
+  "" "DesktopAssistant" "gcc" "CMakeFiles/DesktopAssistant.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
